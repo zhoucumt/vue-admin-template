009d94e5d7db123e287099b3495e27e9a67b032e
zhoucumt <553817478@qq.com>
2018-11-14 09:27:06 +0800

    add babel-polyfill

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,10 @@ module.exports = {
 
     // webpack配置
     // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
-    chainWebpack: () => {},
+    chainWebpack:  config => {
+        config.entry('index').add('babel-polyfill');
+    }
+
 
     configureWebpack: () => {},
 
