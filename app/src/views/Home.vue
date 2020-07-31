b67ed1c9625087c3f1b8ab055edb720f54e1223c
zhoucumt <553817478@qq.com>
2020-07-31 22:22:47 +0800

    树形组件（递归组件）

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -3,21 +3,90 @@
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <div>{{name}}</div>
+    <Tree :data="treeData"></Tree>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import Tree from '@/components/Tree/Tree.vue';
 
 export default {
   name: 'Home',
   components: {
     HelloWorld,
+    Tree,
   },
   data() {
     return {
       name: 'name',
+      treeData: [
+        {
+          title: 'Web全栈架构师',
+          children: [
+            {
+              title: 'Java架构师',
+            },
+            {
+              title: 'JS高级',
+              children: [
+                {
+                  title: 'ES6',
+                },
+                {
+                  title: '动效',
+                },
+              ],
+            },
+            {
+              title: 'Web全栈',
+              children: [
+                {
+                  title: 'Vue训练营',
+                  expand: true,
+                  children: [
+                    {
+                      title: '组件化',
+                    },
+                    {
+                      title: '源码',
+                    },
+                    {
+                      title: 'docker部署',
+                    },
+                  ],
+                },
+                {
+                  title: 'React',
+                  children: [
+                    {
+                      title: 'JSX',
+                    },
+                    {
+                      title: '虚拟DOM',
+                    },
+                  ],
+                },
+                {
+                  title: 'Node',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          title: '大数据工程师',
+          children: [
+            {
+              title: '数据分析',
+            },
+            {
+              title: '数据挖掘',
+            },
+          ],
+        },
+      ],
     };
   },
 };
